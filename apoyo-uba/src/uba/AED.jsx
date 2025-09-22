23064b2bbf024b1ef266e3c7df659e38add82abb
PriAnto06 <priscilaantonella.d.07@gmail.com>
2025-09-22 09:41:29 -0300

    se corrigio un error

--- apoyo-uba/src/uba/AED.jsx
+++ apoyo-uba/src/uba/AED.jsx
@@ -1,7 +1,7 @@
 export default function AED() {
   return (
     <div>
-      <h1>🧩 AAlgoritmos y estructura de datos</h1> 
+      <h1>🧩 Algoritmos y estructura de datos</h1> 
       <p>Aquí encontrarás apuntes y recursos de AED.</p>
     </div>
   );
