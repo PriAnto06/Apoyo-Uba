556ab24647fd40c686c7673a65859f2b9ba23b1b
PriAnto06 <priscilaantonella.d.07@gmail.com>
2025-09-22 15:41:34 -0300

    grilla vinculada

--- apoyo-uba/src/components/Grilla.jsx
+++ apoyo-uba/src/components/Grilla.jsx
@@ -5,51 +5,44 @@ const cards = [
   {
     name: "Matematica",
     img: "https://i.pinimg.com/736x/11/27/fc/1127fc5b3431a9291c71c10445969741.jpg",
-    className: "Fernando Michalski",
   },
   {
     name: "Ingles",
     img: "https://i.pinimg.com/736x/65/34/43/653443bd861cef14463bfb533487e6a8.jpg",
-    className: "Gabriel Veronda",
   },
   {
     name: "Quimica",
     img: "https://i.pinimg.com/1200x/52/22/e3/5222e3de01d704ede991a1842c6e6ab4.jpg",
-    className: "Marcela Gleiser",
-  },{
+  },
+  {
     name: "AGBD",
-    img: "https://i.pinimg.com/736x/11/27/fc/1127fc5b3431a9291c71c10445969741.jpg",
-    className: "Tomas Mayorga",
+    img: "https://i.pinimg.com/736x/f3/78/fe/f378fe13f884fa9417c0ebff36ab05fd.jpg",
   },
   {
     name: "AED",
-    img: "https://i.pinimg.com/736x/65/34/43/653443bd861cef14463bfb533487e6a8.jpg",
-    className: "Mauricio Uribe",
+    img: "https://i.pinimg.com/736x/96/27/93/9627931ce425e9483e7c1c1647f53a5e.jpg",
   },
   {
     name: "TAP",
-    img: "https://i.pinimg.com/1200x/52/22/e3/5222e3de01d704ede991a1842c6e6ab4.jpg",
-    className: "Julian Impelluso",
-  },{
+    img: "https://i.pinimg.com/736x/cf/db/70/cfdb70005d1b9930354fc0166b936b3f.jpg",
+  },
+  {
     name: "Programacion Web",
-    img: "https://i.pinimg.com/736x/65/34/43/653443bd861cef14463bfb533487e6a8.jpg",
-    className: "Federico Villace",
+    img: "https://i.pinimg.com/736x/00/5d/c1/005dc1e96cbd4b8e4755c500c34118bb.jpg",
   },
   {
     name: "Diseño de Software",
-    img: "https://i.pinimg.com/1200x/52/22/e3/5222e3de01d704ede991a1842c6e6ab4.jpg",
-    className: "Andres Navarro",
+    img: "https://i.pinimg.com/736x/3d/6a/6c/3d6a6c4ea071378b66b3c6530af3b78c.jpg",
   },
-  
 ];
 
 export function Grilla() {
   return (
     <div className="grid">
       {cards.map((card, index) => (
-        <div key={index} className={`card ${card.className}`}>
+        <div key={index} className="card">
+          <h2 className="card-title">{card.name}</h2>
           <img src={card.img} alt={card.name} />
-          <h2>{card.name}</h2>
         </div>
       ))}
     </div>
